b162fde3aa473a98033b31f258b8675132fdbaf0
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-10 14:09:37 -0300

    fazendo o main

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import sqlite3 as sqlite
 
+#abre uma conexão com o banco
 conn = sqlite.connect("biblioteca.db")
+
+#cria um cursor (um objeto para interagir com o banco)
 cursor = conn.cursor()
 
-cursor 
+#executa o sql
+cursor.execute("SELECT * FROM usuarios")
+
+#pega os registros e guarda na variável resultados
+resultados = cursor.fetchall()
+
+#percorre os registros que retornaram
+for linha in resultados:
+    print(f"id: {linha[0]} | nome: {linha[1]}")
+
+#fecha a conexão
+conn.close()
