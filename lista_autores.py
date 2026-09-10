09c75c1d3179ad0398576635188ae15abc64b769
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-10 14:16:18 -0300

    listando usando select

--- lista_autores.py
+++ lista_autores.py
@@ -2,19 +2,20 @@ import sqlite3 as sqlite
 
 #abre uma conexão com o banco
 conn = sqlite.connect("biblioteca.db")
+conn.row_factory = sqlite.Row
 
 #cria um cursor (um objeto para interagir com o banco)
 cursor = conn.cursor()
 
 #executa o sql
-cursor.execute("SELECT * FROM usuarios")
+cursor.execute("SELECT * FROM autores")
 
 #pega os registros e guarda na variável resultados
 resultados = cursor.fetchall()
 
 #percorre os registros que retornaram
 for linha in resultados:
-    print(f"id: {linha[0]} | nome: {linha[1]}")
+    print(f"id: {linha['id']} | nome: {linha['nome']}")
 
 #fecha a conexão
 conn.close()
---
+++ lista_editoras.py
@@ -0,0 +1,21 @@
+import sqlite3 as sqlite
+
+#abre uma conexão com o banco
+conn = sqlite.connect("biblioteca.db")
+conn.row_factory = sqlite.Row
+
+#cria um cursor (um objeto para interagir com o banco)
+cursor = conn.cursor()
+
+#executa o sql
+cursor.execute("SELECT * FROM editoras")
+
+#pega os registros e guarda na variável resultados
+resultados = cursor.fetchall()
+
+#percorre os registros que retornaram
+for linha in resultados:
+    print(f"id: {linha['id']} | nome: {linha['nome']}")
+
+#fecha a conexão
+conn.close()
