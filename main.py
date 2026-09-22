d217757206d1d33e5928ae875b45875615bb7054
ramon-heerdt <137829996+ramon-heerdt@users.noreply.github.com>
2026-09-22 14:35:24 -0300

    editado por Miguel

---
+++ main.py
@@ -0,0 +1,119 @@
+while(True):
+    print('\n1 - Usuário')
+    print('2 - Autores')
+    print('3 - Editores')
+    print('4 - Livros')
+    print('5 - Emprestimos')
+    print('6 - Histórico')
+    menu = int(input(('Digite: ')))
+
+    if (menu == 1):
+        while(True):
+            print('\n1 - Cadastrar usuário')
+            print('2 - Listar usuários')
+            print('3 - Voltar')
+            int(input('Digite: '))
+
+            if (menu == 1):
+                pass
+                break
+
+            elif (menu == 2):
+                pass
+                break
+
+            elif (menu == 3):
+                break
+
+            else:
+                print('\nAlgo deu errado!\nTente novamente.')
+
+    elif (menu == 2):
+        while(True):
+            print('\n1 - Cadastrar autor')
+            print('2 - Listar autores')
+            print('3 - Voltar')
+            int(input('Digite: '))
+
+            if (menu == 1):
+                pass
+                break
+
+            elif (menu == 2):
+                pass
+                break
+
+            elif (menu == 3):
+                break
+
+            else:
+                print('\nAlgo deu errado!\nTente novamente.')
+
+    elif (menu == 3):
+        while(True):
+            print('\n1 - Cadastrar editor')
+            print('2 - Listar editores')
+            print('3 - Voltar')
+            int(input('Digite: '))
+
+            if (menu == 1):
+                pass
+                break
+
+            elif (menu == 2):
+                pass
+                break
+
+            elif (menu == 3):
+                break
+
+            else:
+                print('\nAlgo deu errado!\nTente novamente.')
+
+    elif (menu == 4):
+        while(True):
+            print('\n1 - Cadastrar livro')
+            print('2 - Listar livros')
+            print('3 - Voltar')
+            int(input('Digite: '))
+
+            if (menu == 1):
+                pass
+                break
+
+            elif (menu == 2):
+                pass
+                break
+
+            elif (menu == 3):
+                break
+
+            else:
+                print('\nAlgo deu errado!\nTente novamente.')
+
+    elif (menu == 5):
+        while(True):
+            print('\n1 - Fazer emprestimo')
+            print('2 - Listar emprestimos')
+            print('3 - Voltar')
+            int(input('Digite: '))
+
+            if (menu == 1):
+                pass
+                break
+
+            elif (menu == 2):
+                pass
+                break
+
+            elif (menu == 3):
+                break
+
+            else:
+                print('\nAlgo deu errado!\nTente novamente.')
+
+    elif (menu == 6):
+        pass
+        
+    else:
+        print('\nAlgo deu errado!\nTente novamente.')
